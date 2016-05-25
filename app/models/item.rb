659d4783651a3ef4eb8f4ebe88764dd8b5a17b61
AsamotoYuki <asamoto@basicinc.jp>
2016-05-25 02:01:25 +0000

    making have/want

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  has_many :wants , through: :want_users , foreign_key: "item_id"
+  has_many :want_users , through: :wants , source: :user
+  has_many :haves , through: :have_users , foreign_key: "item_id"
+  has_many :have_users , through: :haves , source: :user
 end
