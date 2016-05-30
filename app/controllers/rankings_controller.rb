c0dcffe9568627460ff35eef7f1d762839f93f7e
AsamotoYuki <asamoto@basicinc.jp>
2016-05-30 02:27:58 +0000

    update ranking sql

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -8,11 +8,11 @@ class RankingsController < ApplicationController
   
   def have
     #items.title,items.description,items.detail_page_url,items.small_image,items.medium_image,items.large_image,items.created_at,items.updated_at,items.item_code
-    @items = Item.find_by_sql(['SELECT items.id,items.title,items.description,items.detail_page_url,items.small_image,items.medium_image,items.large_image,items.created_at,items.updated_at,items.item_code FROM items INNER JOIN ownerships ON items.id = ownerships.item_id WHERE ownerships.type = "Have" GROUP BY ownerships.item_id ORDER BY count(type) DESC LIMIT 10'])
+    @items = Item.find_by_sql(['SELECT items.id,items.title,items.description,items.detail_page_url,items.small_image,items.medium_image,items.large_image,items.created_at,items.updated_at,items.item_code FROM items INNER JOIN ownerships ON items.id = ownerships.item_id WHERE ownerships.type == "Have" GROUP BY ownerships.item_id ORDER BY count(type) DESC LIMIT 10'])
   end
   
   def want
-    @items = Item.find_by_sql(['SELECT items.id,items.title,items.description,items.detail_page_url,items.small_image,items.medium_image,items.large_image,items.created_at,items.updated_at,items.item_code FROM items INNER JOIN ownerships ON items.id = ownerships.item_id WHERE ownerships.type = "Want" GROUP BY ownerships.item_id ORDER BY count(type) DESC LIMIT 10'])
+    @items = Item.find_by_sql(['SELECT items.id,items.title,items.description,items.detail_page_url,items.small_image,items.medium_image,items.large_image,items.created_at,items.updated_at,items.item_code FROM items INNER JOIN ownerships ON items.id = ownerships.item_id WHERE ownerships.type == "Want" GROUP BY ownerships.item_id ORDER BY count(type) DESC LIMIT 10'])
   end
   
   def new
